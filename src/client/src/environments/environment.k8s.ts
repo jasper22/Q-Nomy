596562b46d42f9bde527c3c9a5884be2b24ae279
alex <alex@quick-dev.co.il>
2020-05-17 22:25:03 +0300

    Added Ingress service for Angular Fixed kuberentes services

--- src/client/src/environments/environment.k8s.ts
+++ src/client/src/environments/environment.k8s.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // This is Kubernetes address as defined in webapi-service.yaml
-  serverUrl: 'http://webapi-k8s.default.svc.cluster.local:5007/api/v1/Patients'
+  serverUrl: 'http://webapi-frontend:5000/api/v1/Patients'
 };
