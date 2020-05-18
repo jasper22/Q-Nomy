2ee3bd01aa3de1c9a3606b78a5f734915aea336e
alex <alex@quick-dev.co.il>
2020-05-18 10:21:05 +0300

    Fixed error in Ingress controler Added correct connection string in Angular frontend

--- src/client/src/environments/environment.k8s.ts
+++ src/client/src/environments/environment.k8s.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
   // This is Kubernetes address as defined in webapi-service.yaml
-  serverUrl: 'http://webapi-k8s.qnomy-local:5000/api/v1/Patients'
+  // Although .NET Core is listening on port number 5000
+  // the Ingress controller in Minikube can not handle different port number but 80
+  // So redirection is done on Service level and here we asking for port 80
+  serverUrl: 'http://webapi-k8s.qnomy-local/api/v1/Patients'
 };
