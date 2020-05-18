170758173ae3fbaa2812d99d67854a3a94dbf184
alex <alex@quick-dev.co.il>
2020-05-19 01:07:05 +0300

    Fixed incorrect path to main Docker Hub registry

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DOCKER_REGISTRY_HOST="docker.io/library"
+export DOCKER_REGISTRY_HOST="docker.io"
 
-docker-compose -f docker-compose.yml build --parallel --build-arg DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST}
+docker-compose -f docker-compose.yml build --build-arg DOCKER_REGISTRY_HOST=${DOCKER_REGISTRY_HOST}
